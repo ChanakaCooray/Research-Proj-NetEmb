c4f0b9ab2330c20b100e282d55ee7c81d7887870
Chanaka Sampath Cooray <chanakasampath91@gmail.com>
2019-02-09 12:16:16 -0600

    Add multithreading support

--- src/ChromosomeIdentifier.py
+++ src/ChromosomeIdentifier.py
@@ -1,13 +1,10 @@
 import os
-import pandas as pd
-# import threading
-# from concurrent.futures import ThreadPoolExecutor
-# from time import sleep
+from concurrent.futures import ThreadPoolExecutor
 
 # Path for GATC.fends file
 fendsFile = "metadata/GATC.fends"
 # Path for the root directory of the data files
-dataDir = "../data"
+dataDir = "data.dir"
 # path to metadata of Diploids 2i
 metaDip2iFile = "metadata/Diploids_2i.txt"
 # path to metadata of Diploids serum
@@ -16,25 +13,55 @@ metaDipSerumFile = "metadata/Diploids_serum.txt"
 metaHaploidsFile = "metadata/Haploids.txt"
 
 # output directory
-outputDir = os.path.join("../output", "chromosomeMap")
+outputDir = os.path.join("output", "chromosomeMap")
 
 # create output directories if not exist
 if not os.path.exists(outputDir):
     os.makedirs(outputDir)
 
 
-# store metadata of Diploids abd Haploids
+def processFile(subdir, dirName, fends):
+    out = open(os.path.join(outputDir, dirName), "w")
+    out.write("chr1 coord1 chr2 coord2 count\n")
+    with open(os.path.join(subdir, "adj")) as f:
+        print("Processing {}...".format(os.path.join(subdir, "adj")))
+        next(f)
+        for line in f:
+            splitLine = line.split()
+            out.write(fends[splitLine[0]][0] + " " + fends[splitLine[0]][1] + " " + fends[splitLine[1]][0] + " " +
+                      fends[splitLine[1]][1] + " 1\n")
+    out.close()
+
+
+# store metadata of Diploids 2i
+metaDip2i = {}
 print("Processing Metadata {}".format(metaDip2iFile))
-df_Dip2i = pd.read_csv(metaDip2iFile, sep="\t", header=0, index_col=0)
-df_Dip2i.index = df_Dip2i.index.map(lambda x: x.replace("_", "."))
+with open(metaDip2iFile) as f:
+    next(f)
+    for line in f:
+        splitLine = line.split()
+        if splitLine[1] == "1":
+            metaDip2i[splitLine[0]] = (splitLine[1], splitLine[2])
 
+# store metadata of Diploids Serum
+metaDipSerum = {}
 print("Processing Metadata {}".format(metaDipSerumFile))
-df_DipSerum = pd.read_csv(metaDipSerumFile, sep="\t", header=0, index_col=0)
-df_DipSerum.index = df_DipSerum.index.map(lambda x: x.replace("_", "."))
+with open(metaDipSerumFile) as f:
+    next(f)
+    for line in f:
+        splitLine = line.split()
+        if splitLine[1] == "1":
+            metaDipSerum[splitLine[0]] = (splitLine[1], splitLine[2])
 
+# store metadata of Haploids
+metaHap = {}
 print("Processing Metadata {}".format(metaHaploidsFile))
-df_Hap = pd.read_csv(metaHaploidsFile, sep="\t", header=0, index_col=1)
-df_Hap.index = df_Hap.index.map(lambda x: x.replace("_", "."))
+with open(metaHaploidsFile) as f:
+    next(f)
+    for line in f:
+        splitLine = line.split()
+        if splitLine[2] == "1":
+            metaHap[splitLine[1]] = (splitLine[0], splitLine[2], splitLine[3])
 
 # create a map using the GATC.fends file
 print("Processing GATC.fends file...")
@@ -46,30 +73,12 @@ with open(fendsFile) as f:
         fends[splitLine[0]] = (splitLine[1], splitLine[2])
 
 print("Processing data...")
+executor = ThreadPoolExecutor(40)
 for subdir, dirs, files in (os.walk(dataDir)):
     dirName = subdir.split("/")[-1]
+    dirNameTem = dirName.replace(".", "_")
 
-    if dirName not in df_Dip2i.index and dirName not in df_DipSerum.index and dirName not in df_Hap.index:
+    if dirNameTem not in metaDip2i and dirName not in metaDipSerum and dirName not in metaHap:
         continue
 
-    if dirName in df_Dip2i.index:
-        if df_Dip2i.loc[dirName]['passed_qc'] == 0:
-            continue
-
-    if dirName in df_DipSerum.index:
-        if df_DipSerum.loc[dirName]['passed_qc'] == 0:
-            continue
-
-    if dirName in df_Hap.index:
-        if df_Hap.loc[dirName]['passed_qc'] == 0:
-            continue
-
-    out = open(os.path.join(outputDir, dirName), "w")
-    out.write("chr1 coord1 chr2 coord2 count\n")
-    with open(os.path.join(subdir, "adj")) as f:
-        "Processing " + os.path.join(subdir, "adj") + " ..."
-        next(f)
-        for line in f:
-            splitLine = line.split()
-            out.write(fends[splitLine[0]][0] + " " + fends[splitLine[0]][1] + " " + fends[splitLine[1]][0] + " " +
-                      fends[splitLine[1]][1] + " 1\n")
+    executor.submit(processFile, subdir, dirName, fends)
--- src/GenerateEdges.py
+++ src/GenerateEdges.py
@@ -1,10 +1,10 @@
 import os
-import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
 
 # input data file
 dataDir = os.path.join("output", "chromosomeMap")
 # chromosome bins metada
-chrom_bin="metadata/chrom_bins.txt"
+chromBin_file = "metadata/chrom_bins.txt"
 # bin size
 binSize = 100000
 # output directory
@@ -14,16 +14,23 @@ outputDir = os.path.join("output", "EdgesBin")
 if not os.path.exists(outputDir):
     os.makedirs(outputDir)
 
-df_chrombin = pd.read_csv(chrom_bin, sep=" ", header=None, names=["chrm", "startIndex"], index_col=0)
+# store bin indexes
+chromBin = {}
+with open(chromBin_file) as f:
+    for line in f:
+        splitLine = line.split()
+        chromBin[splitLine[0]] = splitLine[1]
 
-for filename in os.listdir(dataDir):
+
+def processFile(filename):
+    print("Processing {}...".format(filename))
     with open(os.path.join(dataDir, filename)) as f:
         next(f)
         chrEntry = {}
         for line in f:
             splitLine = line.split()
 
-            startIndex = df_chrombin.loc[splitLine[0]]['startIndex']
+            startIndex = int(chromBin[splitLine[0]])
 
             # figure out the correct bin
             bin1 = int(splitLine[1]) // binSize + startIndex
@@ -37,8 +44,14 @@ for filename in os.listdir(dataDir):
             else:
                 chrEntry[(bin1, bin2)] = int(splitLine[4])
 
+        outputFile = os.path.join(outputDir, filename)
+        out = open(outputFile, "w")
         for key, val in chrEntry.items():
-            outputFile = os.path.join(outputDir, filename)
-            out = open(outputFile, "a")
             out.write("{} {} {}\n".format(key[0], key[1], val))
-            out.close()
+        out.close()
+
+
+print("Processing Files...")
+executor = ThreadPoolExecutor(40)
+for filename in os.listdir(dataDir):
+    executor.submit(processFile, filename)
