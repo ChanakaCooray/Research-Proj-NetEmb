985837d39ea97bb3b0d884b8209ee24a21d7306d
Chanaka Sampath Cooray <chanakasampath91@gmail.com>
2019-02-20 22:59:26 -0600

    Add analysis for the shift bins

---
+++ scripts/AnalyzeSumMatrix.sh
@@ -0,0 +1,19 @@
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX1 --bin-size 500k --shift 100k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX1 --bin-size 500k --shift 200k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX1 --bin-size 500k --shift 300k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX1 --bin-size 500k --shift 400k
+
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX2 --bin-size 500k --shift 100k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX2 --bin-size 500k --shift 200k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX2 --bin-size 500k --shift 300k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX2 --bin-size 500k --shift 400k
+
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX3 --bin-size 500k --shift 100k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX3 --bin-size 500k --shift 200k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX3 --bin-size 500k --shift 300k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX3 --bin-size 500k --shift 400k
+
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX4 --bin-size 500k --shift 100k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX4 --bin-size 500k --shift 200k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX4 --bin-size 500k --shift 300k
+python src/analyze/AnalyzeSumMatrix.py --cell-type 1CDX4 --bin-size 500k --shift 400k
