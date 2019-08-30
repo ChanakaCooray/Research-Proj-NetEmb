e1da4cf39aea50d64855a226ddf648d32f96582b
Chanaka Sampath Cooray <chanakasampath91@gmail.com>
2019-08-30 15:16:53 -0500

    Add mESC Stat

---
+++ src/tool/mESC-MatchBin.py
@@ -0,0 +1,133 @@
+import os
+from concurrent.futures import ThreadPoolExecutor
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import sys
+import matplotlib.pyplot as plt
+import random
+from statistics import mean
+from statistics import stdev
+
+
+# determine the bin for the given coordinate and the sliding window
+def find_bin(coord, start_index, bin_size, shift):
+    bin_num = coord // bin_size
+    remainder = coord % bin_size
+
+    bin_num += start_index
+
+    if shift == 0:
+        return bin_num
+
+    if remainder >= shift:
+        bin_num += 1
+
+    return bin_num
+
+
+def main():
+    parser = ArgumentParser("EdgeDetector",
+                            formatter_class=ArgumentDefaultsHelpFormatter,
+                            conflict_handler='resolve')
+    parser.add_argument("--cell")
+    parser.add_argument("--type")
+
+    args = parser.parse_args()
+    cell = args.cell
+    type = args.type
+
+    chrom_bin_range = "metadata/chrom_bins_range_500k.txt"
+
+    # search for last index
+    with open(chrom_bin_range) as f:
+        for last in f: pass
+        n = int(last.split()[3]) + 1
+
+    # print(n)
+
+    chrom_bin_file = "metadata/chrom_bins_500k.txt"
+
+    # store bin indexes
+    chrom_bin = {}
+    with open(chrom_bin_file) as f:
+        for line in f:
+            split_line = line.split()
+            chrom_bin["chr" + split_line[0]] = split_line[1]
+
+    mESC_file = "mESC/mESC.{}.txt".format(type)
+    # mESC_file = "mESC/mESC.enhancer.txt"
+    # mESC_file = "mESC/mESC.h3k4me3.peak.txt"
+    # mESC_file = "mESC/mESC.h3k27ac.peak.txt"
+    # mESC_file = "mESC/mESC.polII.peak.txt"
+
+    sig_bin_file = "output/tool-output-v2/{}_500k_max.txt".format(cell)
+    # sig_bin_file = "output/tool-output-v2/1CDX2_500k_max.txt"
+    # sig_bin_file = "output/tool-output-v2/1CDX3_500k_max.txt"
+    # sig_bin_file = "output/tool-output-v2/1CDX4_500k_max.txt"
+
+    print(mESC_file)
+    print(sig_bin_file)
+
+    mESC_bin_map = {}
+
+    for i in range(0, n + 1):
+        mESC_bin_map[i] = 0
+
+    with open(mESC_file) as f:
+        for line in f:
+            split_line = line.split()
+            bin_n = find_bin(int(split_line[1]), int(chrom_bin[split_line[0]]), 500000, 0)
+
+            mESC_bin_map[bin_n] += 1
+
+    bin_set = set()
+
+    with open(sig_bin_file) as f:
+        next(f)
+        for line in f:
+            split_line = line.split()
+            bin_set.add(int(split_line[0]))
+            bin_set.add(int(split_line[1]))
+
+    # sig_bin_values = []
+
+    n_value = 0
+    for j in bin_set:
+        # sig_bin_values.append(mESC_bin_map[j])
+        n_value += mESC_bin_map[j]
+
+    # n_value = sum(sig_bin_values)
+
+    print("Unique: " + str(len(bin_set)))
+    print(n_value)
+
+    all_bin_list = list(mESC_bin_map.keys())
+
+    random_value_list = []
+    for k in range(0, 10000):
+        random_list = random.sample(all_bin_list, len(bin_set))
+
+        random_value = 0
+        for j in random_list:
+            random_value += mESC_bin_map[j]
+
+        random_value_list.append(random_value)
+
+    print(mean(random_value_list))
+    print(stdev(random_value_list))
+
+    z_score = (n_value - mean(random_value_list)) / stdev(random_value_list)
+
+    print(z_score)
+
+    # all_bin_values = list(mESC_bin_map.values())
+
+    # fig = plt.figure(figsize=(8,6))
+    # plt.boxplot([x for x in [sig_bin_values,all_bin_values]], 0, 'rs', 1)
+    # plt.xticks([y+1 for y in range(len([sig_bin_values,all_bin_values]))], ['Significant Bins','All Bins'])
+    # plt.xlabel('measurement x')
+    # t = plt.title('Box plot')
+    # plt.show()
+
+
+if __name__ == '__main__':
+    main()
